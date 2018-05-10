de45b37d3707086365aa806e77ea2ba49557dd8a
Alrescha <hase-takatoshi-cm@ynu.jp>
2018-05-10 15:41:21 +0900

    ファイル作成

---
+++ ex_SWLED_VERILOG.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    16:55:37 12/12/2017 
+// Design Name: 
+// Module Name:    ex_SWLED_VERILOG 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module ex_SWLED_VERILOG(
+    input CLK,
+    output LED0
+    );
+
+assign LED0 = CLK;
+
+endmodule
+
